fcbb7d14ec652fd36a1d9446389ce163305c14f4
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-03-16 11:09:53 +0100

    feat: setup react scaffolding for Inertia 1.0

--- template/config/react/assets/js/app.js
+++ template/config/react/assets/js/app.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createInertiaApp } from '@inertiajs/inertia-react'
-import { InertiaProgress } from '@inertiajs/progress'
+import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from '@inertiajs/react'
 import '~/css/main.css'
-
-InertiaProgress.init()
 
 createInertiaApp({
   resolve: (name) => require(`./pages/${name}`),
   setup({ el, App, props }) {
-    render(<App {...props} />, el)
+    createRoot(el).render(<App {...props} />)
   },
+  progress: {
+    color: '#29d'
+  }
 })
