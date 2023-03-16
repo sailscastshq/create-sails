a7a525e2b1b9d1c40891790d05da1986e0cf715f
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-03-16 10:45:02 +0100

    feat: update setup code for vue

--- template/config/vue/assets/js/app.js
+++ template/config/vue/assets/js/app.js
@@ -1,15 +1,16 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue3'
-import { InertiaProgress } from '@inertiajs/progress'
-import '~/css/main.css';
-
-InertiaProgress.init()
+import { createInertiaApp } from '@inertiajs/vue3'
+import '~/css/main.css'
 
 createInertiaApp({
-  resolve: name => require(`./pages/${name}`),
+  title: 'New App',
+  resolve: (name) => require(`./pages/${name}`),
   setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props)})
+    createApp({ render: () => h(App, props) })
       .use(plugin)
       .mount(el)
   },
+  progress: {
+    color: '#29d'
+  }
 })
