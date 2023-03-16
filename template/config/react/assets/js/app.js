ebfe388ee5e3dc9620eb38b72e8a452063c22f21
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-03-16 11:18:27 +0100

    chore: add title for the React setup

--- template/config/react/assets/js/app.js
+++ template/config/react/assets/js/app.js
@@ -3,6 +3,7 @@ import { createInertiaApp } from '@inertiajs/react'
 import '~/css/main.css'
 
 createInertiaApp({
+  title: 'RITS stack',
   resolve: (name) => require(`./pages/${name}`),
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
